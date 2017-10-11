e12749fa9988e9f21342e2fa599168f3154d64a6
oleg <olegatorius1@gmail.com>
2017-10-11 13:00:13 +0300

    js_dz3

---
+++ js/js.js
@@ -0,0 +1,104 @@
+
+/* First Task*/
+
+var arr =[];
+
+var el = document.querySelector('#new');
+
+function arrView (){
+
+// var index = +document.querySelector('#index');
+
+// var value = +document.querySelector('#value');
+
+// var array = ['one', 'two', 'three', 'four', 'five']; 
+ 
+  
+   el.innerHTML = arr;
+
+    }
+var button = document.querySelector('button');
+
+button.addEventListener("click", arrView);
+
+
+/* next task submit*/
+
+// var arr = [];
+
+var submitButton = document.querySelector('#submitButton');
+
+submitButton.addEventListener("click", submitForm);
+
+function submitForm (){
+
+var index = document.querySelector('#index');
+
+var value = document.querySelector('#value');
+
+var ReadyIndex = parseInt(index.value);
+
+arr[ReadyIndex] = value.value;
+
+alert(arr);
+
+}
+
+/*next task pop*/
+
+
+
+var popButton = document.querySelector('#popButton');
+
+popButton.addEventListener("click", popAray);
+
+function popAray (){
+
+arr.pop();
+
+alert(arr);
+
+}
+
+/* next task push*/
+
+var pushButton = document.querySelector('#pushButton');
+
+pushButton.addEventListener("click", pushAray);
+
+function pushAray (){
+
+arr.push(value.value);
+
+alert(arr);
+
+}
+
+/* next task shift */ 
+
+
+var shiftButton = document.querySelector('#shiftButton');
+
+shiftButton.addEventListener("click", shiftAray);
+
+function shiftAray (){
+
+arr.shift();
+
+alert(arr);
+
+}
+
+/* next task unshift */
+
+var unshiftButton = document.querySelector('#unshiftButton');
+
+unshiftButton.addEventListener("click", unshiftAray);
+
+function unshiftAray (){
+
+arr.unshift(value.value);
+
+alert(arr);
+
+}
